50ec6a7fcde9565160b4629eba4dc24a5c6f2954
Chelsey Morris <chelseymorris91@yahoo.com>
2021-03-16 11:22:24 -0700

    added functions

--- greatBay.js
+++ greatBay.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const mysql = require('inquirer');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -15,8 +15,68 @@ const connection = mysql.createConnection({
   database: 'greatBayDB',
 });
 
+const createItem = () => {
+  console.log('Inserting a new item...\n');
+  const query = connection.query(
+    'INSERT INTO bid SET ?',
+    {
+      postItem: 'user input',
+      bid: 'user input',
+      category: 'user input',
+    },
+    (err, res) => {
+      if (err) throw err;
+      console.log(`${res.affectedRows} item inserted!\n`);
+      // Call updateProduct AFTER the INSERT completes
+      readItems();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+};
+const updateItem = () => {
+  console.log('Updating bid amount...\n');
+  const query = connection.query(
+    'UPDATE bid SET ? WHERE ?',
+    [
+      {
+        bid: 'user input',
+      },
+      {
+        postItem: 'user input',
+      },
+    ],
+  
+    (err, res) => {
+      if (err) throw err;
+      console.log(`${res.affectedRows} bid updated!\n`);
+      // Call readItems AFTER the UPDATE completes
+      readItems();
+    }
+    );
+}
+  const readItems = () => {
+    console.log('Selecting all products...\n');
+    connection.query('SELECT * FROM bid', (err, res) => {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      
+    });
+  };
+
+  const exit =() =>{
+
+    connection.end();
+  }
+  
+  
+
 // Connect to the DB
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
-  });
+    createItem();
+    
+  })
